ad700b42dba09f1515835ef803fe7f7db1462b5e
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-06-12 14:18:02 -0300

    att Transacoes

--- src/bootSantander/intermediario/UltimaTransacao.java
+++ src/bootSantander/intermediario/UltimaTransacao.java
@@ -9,18 +9,18 @@ public class UltimaTransacao {
         String[] partes = entrada.split(",");
 
         // TODO: Solicitar ao usuário que forneca os valores necessários para criar uma Transacao.
-        Transacao transacao = new Transacao(partes[0], partes[1], partes[2], Double.valueOf(partes[3]).doubleValue());
+        Transacao1 transacao = new Transacao1(partes[0], partes[1], partes[2], Double.valueOf(partes[3]).doubleValue());
         transacao.imprimir();
     }
 }
 
-class Transacao {
+class Transacao1 {
     private String data;
     private String hora;
     private String descricao;
     private double valor;
 
-    public Transacao(String data, String hora, String descricao, double valor) {
+    public Transacao1(String data, String hora, String descricao, double valor) {
         this.data = data;
         this.hora = hora;
         this.descricao = descricao;
