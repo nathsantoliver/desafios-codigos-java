0b4a9ca6699f8eeed5900035cd784e94add5f0d9
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-06-12 14:17:36 -0300

    add Registro de Transacoes Bancarias com Stream

---
+++ src/bootSantander/intermediario/RegistroTransacoesComStream.java
@@ -0,0 +1,66 @@
+package bootSantander.intermediario;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class RegistroTransacoesComStream {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<Transacao> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            char tipoTransacao = scanner.next().charAt(0);
+
+            double valorTransacao = scanner.nextDouble();
+
+            Transacao transacao = new Transacao(tipoTransacao, valorTransacao);
+            transacoes.add(transacao);
+
+            // Aqui é atualizado o saldo da conta com base no tipo de transação
+            if (transacao.getTipo() == 'D' || transacao.getTipo() == 'd') {
+                saldo += valorTransacao;
+            } else if (transacao.getTipo() == 'S' || transacao.getTipo() == 's') {
+                saldo -= valorTransacao;
+            }
+        }
+
+        // Exibe o saldo final e a lista de transações ao final do processo usando Stream API
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Transacoes:");
+
+        // Crie um fluxo (stream) a partir da lista de transações:
+        // Mapeie cada transação para uma string formatada:
+        // Colete os elementos do fluxo em uma lista:
+        // Para cada elemento da lista, imprime no console:
+        Stream<Transacao> stream = transacoes.stream();
+        stream.forEach(
+                transacao -> System.out.println(transacao.getTipo() + " de " + transacao.getValor())
+        );
+
+        scanner.close();
+    }
+}
+
+class Transacao {
+    private char tipo;
+    private double valor;
+
+    public Transacao(char tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
