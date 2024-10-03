f0f70a56e95f765d3390ba3e96ce5cc47696d6ee
Nathália Oliveira <nathaliaolisantana@gmail.com>
2024-10-03 15:41:47 -0300

    desafio Gerenciamento de Funcionarios

---
+++ src/bootDeal/intermediario/GerenciamentoDeFuncionarios.java
@@ -0,0 +1,48 @@
+package bootDeal.intermediario;
+
+import java.util.*;
+
+public class GerenciamentoDeFuncionarios {
+    static class Funcionario {
+        int id;
+        String nome;
+        String cargo;
+
+        Funcionario(int id, String nome, String cargo) {
+            this.id = id;
+            this.nome = nome;
+            this.cargo = cargo;
+        }
+
+        @Override
+        public String toString() {
+            return id + ": " + nome + " - " + cargo;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < n; i++) {
+
+            String nome = scanner.nextLine();
+            String cargo = scanner.nextLine();
+
+            // Adicione funcionários à lista 'funcionarios':
+            int id = i+1;
+            Funcionario func = new Funcionario(id, nome, cargo);
+            funcionarios.add(func);
+        }
+
+
+        Collections.sort(funcionarios, Comparator.comparingInt(f -> f.id));
+        for (Funcionario f : funcionarios) {
+            System.out.println(f);
+        }
+    }
+}
